125bb884a8262bb119f88be6f2e66f8b54c5d6f2
ellagrady <emg00150@students.stir.ac.uk>
2024-04-10 12:29:11 -0400

    first commit to Hello-World branch

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# basic hello world function
+# edit this function to print whatever you want
+# once you edit this file, we will use this to learn how to commit changes to your repo
+
+def hello_world(strInput):
+    print(strInput)
+
+if __name__== "__main__":
+    print("hello_world function\nPrinted output: ")
+    hello_world("Hello World!")
